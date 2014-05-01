4877badc7979e32846533ad66980e36b4959c264
Karl O'Neill <karl_oneill@yahoo.com>
2014-05-01 21:38:50 +1000

    Load StatusFragment directly instead of using a StatusActivity XML file

--- Yamba/src/com/way/karlitos/yamba/StatusActivity.java
+++ Yamba/src/com/way/karlitos/yamba/StatusActivity.java
@@ -2,21 +2,24 @@ package com.way.karlitos.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
 
 public class StatusActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.new_activity_status);
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
+		
+		// Check if this activity was created before
+		if (savedInstanceState == null) {
+			// Create a fragment
+			StatusFragment fragment = new StatusFragment();
+			getFragmentManager()
+			.beginTransaction()
+			.add(android.R.id.content, fragment,
+					fragment.getClass().getSimpleName())
+					.commit();
+		}
 	}
 }
