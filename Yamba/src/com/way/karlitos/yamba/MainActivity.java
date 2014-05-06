ee7cf2a7638bf88c31a667b25a1e74bca3f1566e
Karl O'Neill <karl_oneill@yahoo.com>
2014-05-06 16:46:42 +1000

    Finished chapter 12

---
+++ Yamba/src/com/way/karlitos/yamba/MainActivity.java
@@ -0,0 +1,78 @@
+package com.way.karlitos.yamba;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction().add(
+					android.R.id.content,
+					new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			break;
+		case R.id.action_tweet:
+			startActivity(new Intent(this, StatusActivity.class));
+			break;
+		case R.id.action_refresh:
+			startService(new Intent(this, RefreshService.class)); //
+			break;
+		case R.id.action_purge:
+			int rows = getContentResolver().delete(
+			StatusContract.CONTENT_URI, null, null);
+			Toast.makeText(this, "Deleted "+rows+" rows", Toast.LENGTH_LONG).show();
+			return true;
+		default:
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
